e0309e8286941a06d1b9c0d31a8a4382eec7bfae
hosin <didghtls11@gmail.com>
2024-08-16 21:58:36 +0900

    [Comment] 인터셉터에 관한 주석 추가
    - 인터셉터에 관한 주석 추가

--- src/main/java/flow/assignment/interceptor/ExtensionHandlerInterceptor.java
+++ src/main/java/flow/assignment/interceptor/ExtensionHandlerInterceptor.java
@@ -19,12 +19,23 @@ import java.io.InputStream;
 import java.util.Collection;
 import java.util.Optional;
 
+/**
+ * 확장자 관련 Interceptor
+ */
 @Component
 @RequiredArgsConstructor
 public class ExtensionHandlerInterceptor implements HandlerInterceptor {
 
     private final ExtensionRepository extensionRepository;
 
+    /**
+     * 파일 데이터의 확장자를 처리하기 위한 preHandle 메서드
+     * @param request
+     * @param response
+     * @param handler
+     * @return boolean
+     * @throws Exception
+     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Collection<Part> result = request.getParts();
@@ -49,6 +60,11 @@ public class ExtensionHandlerInterceptor implements HandlerInterceptor {
         return false;
     }
 
+    /**
+     * 요청으로 받은 파일의 확장자를 DB에 조회하여 해당 확장자가 차단되어있는지 필터링하는 메서드
+     * @param extension
+     * @return Boolean
+     */
     private Boolean extensionFilter(String extension) {
         Optional<Extension> extensionInfo = extensionRepository.findByExtension(extension);
         if (extensionInfo.isPresent()) {
@@ -63,6 +79,11 @@ public class ExtensionHandlerInterceptor implements HandlerInterceptor {
         }
     }
 
+    /**
+     * 요청으로 받은 파일의 Content-Type의 MimeType을 확인하는 메서드
+     * @param mimeType
+     * @return String
+     */
     private String mimeTypeFilter(String mimeType) {
         String validMimeType = MimeType.isValidMimeType(mimeType);
         if (validMimeType == null) {
@@ -72,6 +93,12 @@ public class ExtensionHandlerInterceptor implements HandlerInterceptor {
         }
     }
 
+    /**
+     * 요청으로 받은 파일의 확장자와 시그니처 정보를 필터링해주는 메서드
+     * @param extension
+     * @param fileSignature
+     * @return Boolean
+     */
     private Boolean fileSignatureFilter(String extension, String fileSignature) {
         Boolean signature = FileSignature.isEqualsSignature(extension, fileSignature);
         if (!signature) {
@@ -81,6 +108,11 @@ public class ExtensionHandlerInterceptor implements HandlerInterceptor {
         }
     }
 
+    /**
+     * 요청으로 받은 파일의 확장자를 통해 일반 텍스트 파일인지 확인하는 메서드
+     * @param extension
+     * @return Boolean
+     */
     private Boolean isGeneralFile(String extension) {
         Boolean generalFile = GeneralFile.isEqualsGeneralFile(extension);
         if (generalFile == null) {
@@ -90,6 +122,12 @@ public class ExtensionHandlerInterceptor implements HandlerInterceptor {
         }
     }
 
+    /**
+     * 요청으로 받은 파일을 읽어 byte 값을 16진수로 변환해 해당 파일의 시그니처 값을 구하는 메서드
+     * @param inputStream
+     * @return String
+     * @throws IOException
+     */
     private String convertByteToSignature(InputStream inputStream) throws IOException {
         byte[] bytes = inputStream.readNBytes(2);
         String signature = "";
