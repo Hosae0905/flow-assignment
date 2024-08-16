11079a531e5d27cef9500c88bb162b249ca63619
hosin <didghtls11@gmail.com>
2024-08-16 22:18:21 +0900

    [Comment] BaseResponse 관련 주석 추가
    - BaseResponse 관련 주석 추가

--- src/main/java/flow/assignment/common/BaseResponse.java
+++ src/main/java/flow/assignment/common/BaseResponse.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 공통적인 성공 응답에 관한 BaseResponse 클래스
+ * @param <T>
+ */
 @AllArgsConstructor
 @Getter
 @Setter
@@ -14,6 +18,14 @@ public class BaseResponse<T> {
     private String message;
     private T result;
 
+    /**
+     * 성공 응답 객체를 생성하는 메서드
+     * @param code
+     * @param isSuccess
+     * @param message
+     * @param result
+     * @return <T>BaseResponse<T>
+     */
     public static <T>BaseResponse<T> successResponse(String code, Boolean isSuccess, String message, T result) {
         return new BaseResponse<>(code, isSuccess, message, result);
     }
