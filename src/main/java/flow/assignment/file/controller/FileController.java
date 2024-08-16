7072b5db113e99ddb57ffa8cd204f2e4ef3811dc
hosin <didghtls11@gmail.com>
2024-08-16 23:46:00 +0900

    [Feat] SpringDoc 적용
    - SpringDoc를 적용하여 swagger-ui를 통한 api 테스트 환경 구축

--- src/main/java/flow/assignment/file/controller/FileController.java
+++ src/main/java/flow/assignment/file/controller/FileController.java
@@ -2,6 +2,12 @@ package flow.assignment.file.controller;
 
 import flow.assignment.common.BaseResponse;
 import flow.assignment.file.service.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,10 +20,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/file")
 @RequiredArgsConstructor
+@Tag(name = "FileUpload", description = "FileUpload API")
 public class FileController {
     private final FileService fileService;
 
     @RequestMapping(method = RequestMethod.POST, value = "/upload")
+    @Operation(summary = "파일 업로드")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공",
+            content = {@Content(schema = @Schema(implementation = BaseResponse.class))})
+    })
     public ResponseEntity<BaseResponse<String>> fileUpload(MultipartFile file) {
         return ResponseEntity.ok().body(fileService.uploadFile(file));
     }
